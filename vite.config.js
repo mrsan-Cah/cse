99fd5577a1d5d729c9fbee6d18fa616bc36d21ce
mrsan-Cah <senthamizhaselvi@gmail.com>
2025-04-17 15:39:13 +0530

    summa commmits

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/mrsan-Cah/cse.git", // 👈 Change this to your GitHub repo name
+  base: "https://github.com/mrsan-Cah/cse.git", 
   plugins: [react(), tailwindcss()],
 });
